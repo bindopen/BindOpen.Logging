af30ee52113c7217e11fb09c6168cd4f3a8101ec
xgonier <xgonier@meltingsoft.com>
2023-01-02 11:29:53 +0100

    - Upgrading kernel packages

---
+++ src/BindOpen.Logging.Abstractions/Events/IBdoConditionalEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace BindOpen.Logging
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBdoConditionalEvent : IBdoEvent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string ConditionScript { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conditionScript"></param>
+        /// <returns></returns>
+        IBdoConditionalEvent WithCondition(string conditionScript);
+    }
+}
---
+++ src/BindOpen.Logging.Abstractions/Events/IBdoLogEventStackTrace.cs
@@ -0,0 +1,33 @@
+﻿namespace BindOpen.Logging
+{
+    /// <summary>
+    /// This interface defines a log event stack trace.
+    /// </summary>
+    public interface IBdoLogEventStackTrace
+    {
+        /// <summary>
+        /// The name of the full class.
+        /// </summary>
+        string FullClassName { get; set; }
+
+        /// <summary>
+        /// The name of the called method.
+        /// </summary>
+        string MethodName { get; set; }
+
+        /// <summary>
+        /// Parameters of the called method.
+        /// </summary>
+        string MethodParams { get; set; }
+
+        /// <summary>
+        /// Path of the called file.
+        /// </summary>
+        string FilePath { get; set; }
+
+        /// <summary>
+        /// Called line number.
+        /// </summary>
+        string LineNumber { get; set; }
+    }
+}
